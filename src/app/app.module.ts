a6744d1ed7a0e07692aae47220394ad2e86b9fc0
Cesar <csr2453@outlook.es>
2024-11-12 16:11:23 -0400

    Ejemplo directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from "./hijo/hijo.component";
 import { ContadorComponent } from './contador/contador.component';
 import { FormsModule } from '@angular/forms';
+import { EstiloHermanosDirective } from './estilo-hermanos.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     PadreComponent,
     HijoComponent,
-    ContadorComponent
+    ContadorComponent,
+    EstiloHermanosDirective
   ],
   imports: [
     BrowserModule,
